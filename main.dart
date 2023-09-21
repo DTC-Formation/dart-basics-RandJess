312397bb293b68415117a13e64307c35628398b7
Jess <jessicarandrianasolo53@gmail.com>
2023-09-21 13:33:55 +0300

    voyelle et consonne

--- main.dart
+++ main.dart
@@ -14,15 +14,41 @@ int puissance(int a, int b) {
   return res;
 }
 
+//Mampiditra lettre de mamoka hoe consonne sa voyelle le izy
+// anaty tableau ilay consonne ou autre retra dia zay tsy ao tode voyelle
+var voyelle = ["a", "e", "i", "o", "u", "y"];
 isUppercase(String str) {
-  var dict = [];
-  for (var i = 0; i < str.length; i++) {
-    dict.add(str.substring(i));
-  }
+  var strLowerCase = str.toLowerCase();
+  var dict = []; //au final ceci va renfermer chaque lettre du mot
+  // for (var i = 0; i < str.length; i++) {
+  //   dict.add(str.substring(i));
+  // }
   // String mapStr = str.substring(0);
   //str == str.toUpperCase()
 
-  return dict;
+  // for (var el in str) {
+  //   dict.add(el);
+  // }
+  bool statCase = false;
+
+  for (var i = 0; i < strLowerCase.length; i++) {
+    dict.add(strLowerCase[i]);
+    for (var el in voyelle) {
+      if (strLowerCase[i] == el) {
+        statCase = true;
+        print('object');
+      } else {
+        print('tsy object');
+      }
+    }
+    // if (statCase) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+
+    return i;
+  }
 }
 
 void main() {
